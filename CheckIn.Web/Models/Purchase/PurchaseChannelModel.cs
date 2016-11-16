18f88fe7d0585bf96c238ddfe01b010b7f0f8ec8
Pavan Gonagur <pgonagur@vmware.com>
2016-11-16 23:23:32 +0530

    Purchase Stuff

---
+++ CheckIn.Web/Models/Purchase/PurchaseChannelModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckIn.Web.Models.Purchase
+{
+    public class PurchaseChannelModel
+    {
+        public string AdminName { get; set; }
+
+        public string Email { get; set; }
+
+        public string ChannelName { get; set; }
+
+        public bool IsPublic { get; set; }
+    }
+}
